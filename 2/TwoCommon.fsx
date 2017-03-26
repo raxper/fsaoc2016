cfd846603e45d3725e091e2498ff90c23d2d6112
Sachin V. Shah <svshah@redmane.net>
2017-03-26 10:51:54 -0500

    Added fscheck tests for direction, vertex, edge

--- 2/TwoCommon.fsx
+++ 2/TwoCommon.fsx
@@ -1,5 +1,4 @@
 // Constants
-[<LiteralAttribute>]
 let NO_VERTEX = "."
 
 /// Direction
@@ -14,23 +13,23 @@ module Direction =
 
 /// Vertex
 type Vertex = | V of int * string with
-  override x.ToString() = match x with | V(_, s) -> sprintf "%A" s
+  override x.ToString() = match x with | V(_, s) -> s
 module Vertex =
   let create idx label = V(idx, label)
   let toInt = function | V(x, _) -> x
-  let toString x = x.ToString()
+  let toString (x:Vertex) = x.ToString()
   let strToV lbl = Seq.find (fun (V(_, l)) -> l = lbl)
   let intToV idx = Seq.find (fun (V(i, _)) -> i = idx)
 
 /// Edge
 type Edge = | E of Vertex * Direction * Vertex with
   override x.ToString() =
-    match x with E(v1, d, v2) -> sprintf "%A, %A, %A" v1 d v2
+    match x with E(v1, d, v2) -> sprintf "%A->%A->%A" v1 d v2
 module Edge =
   let create dir (v1, v2) = E(v1, dir, v2)
+  let toString (e:Edge) = e.ToString()
 
-/// Parse a string and return a list of vertices and edges.
-let parseBoard (strB:string)=
+module Parser =
   let transpose rows =
     let numRowsO = Seq.length rows
     let numColsO = Seq.item 0 rows |> Seq.length
@@ -41,27 +40,47 @@ let parseBoard (strB:string)=
             yield (Seq.item j rows |> Seq.item i)
         }
     }
+
   let validEdge (l1, l2) = l1 <> NO_VERTEX && l2 <> NO_VERTEX
+
   let potentialEdges verts rows =
     rows
-    |> Seq.map Seq.pairwise
-    |> Seq.concat
+    |> Seq.collect Seq.pairwise
     |> Seq.filter validEdge
     |> Seq.map
       (fun (vl1, vl2) -> Vertex.strToV vl1 verts, Vertex.strToV vl2 verts)
+
   let reverse (x, y) = y, x
 
-  let rows = strB.Split('\n') |> Seq.map (Seq.map string)
-  let cols = transpose rows
+  let squareup strs =
+    seq {
+      let longestRow = strs |> Seq.map Seq.length |> Seq.max
+      for i = 0 to Seq.length strs-1 do
+        let curStr = Seq.item i strs
+        if Seq.length curStr = longestRow then
+          yield curStr
+        else
+          yield (Seq.replicate (longestRow - Seq.length curStr) "."
+                |> Seq.append curStr)
+    }
 
-  let verts =
-    rows |> Seq.concat |> Seq.filter ((<>) NO_VERTEX) |> Seq.mapi Vertex.create
+  let parseBoard (strB:string)=
+    let strs = strB.Split('\n') |> Seq.map (Seq.map string)
+    let rows = squareup strs
+    let cols = transpose rows
 
-  let redges = potentialEdges verts rows |> Seq.map (Edge.create Direction.R)
-  let ledges = potentialEdges verts rows |> Seq.map reverse
-               |> Seq.map (Edge.create Direction.L)
-  let dedges = potentialEdges verts cols |> Seq.map (Edge.create Direction.D)
-  let uedges = potentialEdges verts cols |> Seq.map reverse
-               |> Seq.map (Edge.create Direction.U)
-  let edges = Seq.append redges ledges |> Seq.append dedges |> Seq.append uedges
-  verts, edges
+    let verts =
+      rows |> Seq.concat |> Seq.filter ((<>) NO_VERTEX) |> Seq.mapi Vertex.create
+
+    let redges = potentialEdges verts rows |> Seq.map (Edge.create Direction.R)
+    let ledges = potentialEdges verts rows |> Seq.map reverse
+                 |> Seq.map (Edge.create Direction.L)
+    let dedges = potentialEdges verts cols |> Seq.map (Edge.create Direction.D)
+    let uedges = potentialEdges verts cols |> Seq.map reverse
+                 |> Seq.map (Edge.create Direction.U)
+    let edges =
+      Seq.append redges ledges
+      |> Seq.append dedges
+      |> Seq.append uedges
+
+    verts, edges
---
+++ 2/TwoCommonTest.fsx
@@ -0,0 +1,85 @@
+#r @"./packages/FsCheck/lib/net452/FsCheck.dll"
+#load "./TwoCommon.fsx"
+
+open FsCheck
+open TwoCommon
+open System
+
+type DirectionTest () =
+
+  static member OppositeDirIsDiff (dir:Direction) =
+    Direction.opposite dir <> dir
+
+  static member OppositesMatch (dir:Direction) =
+    Direction.opposite dir = Direction.opposite dir
+
+  static member OppositeOppositeDirIsSame (dir:Direction) =
+    (Direction.opposite dir |> Direction.opposite) = dir
+
+type VertexTest () =
+  static member Create (i:int) (l:string) =
+    (not (isNull l)) ==> (V(i, l) = Vertex.create i l)
+
+  static member ToInt (i:int) (l:string) =
+    let v = Vertex.create i l
+    (not (isNull l)) ==> (i = Vertex.toInt v)
+
+  static member ToString (i:int) (l:string) =
+    let v = Vertex.create i l
+    (not (isNull l)) ==> (l = Vertex.toString v)
+
+  static member StrToV (i:int) (l:string) =
+    let v = Vertex.create i l
+    let sv = Seq.singleton v
+    (not (isNull l)) ==> (v = Vertex.strToV l sv)
+
+  static member IntToV (i:int) (l:string) =
+    let v = Vertex.create i l
+    let sv = Seq.singleton v
+    (not (isNull l)) ==> (v = Vertex.intToV i sv)
+
+type EdgeTest () =
+  static member Create (i1, l1) (i2, l2) (d:Direction) =
+    let v1 = V(i1, l1)
+    let v2 = V(i2, l2)
+    (not (isNull l1) && not (isNull l2))
+      ==> (E(v1, d, v2) = Edge.create d (v1, v2))
+
+  static member ToString (i1, l1) (i2, l2) (d:Direction) =
+    let v1 = V(i1, l1)
+    let v2 = V(i2, l2)
+    let e = E(v1, d, v2)
+    (not (isNull l1) && not (isNull l2))
+      ==> (Edge.toString e = sprintf "%A->%A->%A" v1 d v2)
+
+type ParserTest () =
+  static member TWVLHelper s1 s2 s3 s4 s5 =
+    let s =
+      sprintf "%s%s%s%s%s%s%s%s%s"
+        s1 (Environment.NewLine)
+        s2 (Environment.NewLine)
+        s3 (Environment.NewLine)
+        s4 (Environment.NewLine)
+        s5
+    let longest =
+      [s1; s2; s3; s4; s5]
+      |> Seq.map Seq.length
+      |> Seq.max
+    longest =
+      (
+        s.Split([|Environment.NewLine|], StringSplitOptions.None)
+        |> Parser.transpose
+        |> Seq.length
+      )
+
+  static member TransposeWidthVsLength s1 s2 s3 s4 s5 =
+    (not (isNull s1) && not (isNull s2) && not (isNull s3) && not (isNull s4) &&  not (isNull s5))
+      ==> (ParserTest.TWVLHelper s1 s2 s3 s4 s5)
+
+(*
+let conf = { Config.Quick with MaxTest = 10000; MaxFail = 5000; Name = "10k" }
+Check.All (conf, typeof<DirectionTest>)
+Check.All (conf, typeof<VertexTest>)
+Check.All (conf, typeof<EdgeTest>)
+Check.All (conf, typeof<ParserTest>)
+*)
