a007e0c56ee1feff0847a7df5bc69e9036c33ea5
Sachin V. Shah <svshah@redmane.net>
2017-04-13 23:49:00 -0500

    Minor tweaks

--- 3/ThreeMain.fsx
+++ 3/ThreeMain.fsx
@@ -1,10 +1,10 @@
 (*
-#load @"c:\Users\SVShah\Projects\fsaoc2016\3\ThreeMain.fsx";; open ThreeMain;;
+#load @"./ThreeMain.fsx";; open ThreeMain;;
 *)
 
 (*
 --- Day 3: Squares With Three Sides ---
-http://adventofcode.com/2016/day/3 
+http://adventofcode.com/2016/day/3
 *)
 
 (*
@@ -23,32 +23,24 @@ open System.IO
 open System.Text.RegularExpressions
 
 (* Applies comparison to one possible triangle's sides *)
-let possibleTriangle sOne sTwo sThree : bool =
-  let triangleComparison longerSide shorterSides = longerSide < shorterSides
-  let inOrderOfLength = [sOne; sTwo; sThree] |> List.sort
-  let sumOfShorter = inOrderOfLength.[0] + inOrderOfLength.[1]
-  let longestSide = inOrderOfLength.[2]
-  triangleComparison longestSide sumOfShorter 
+let possibleTriangle ptr =
+  ptr
+  |> Seq.sort
+  |> fun x -> (Seq.item 0 x) + (Seq.item 1 x) > (Seq.item 2 x)
 
 (* Batch driver to evaluate input file *)
-let possibleTrianglesCount triangleEvaluator = 
-  File.ReadLines @"3\puzzle_input"
+let possibleTrianglesCount triangleEvaluator =
+  File.ReadLines @"./puzzle_input"
   |> Seq.map
     ((fun line -> Regex.Matches (line, @"[0-9]+"))
     >> Seq.cast
     >> Seq.map
       (fun (x : System.Text.RegularExpressions.Match) -> x.Value |> int)
     )
-  |> Seq.filter 
-    (fun il -> 
-      triangleEvaluator 
-        (Seq.item 0 il) 
-        (Seq.item 1 il)
-        (Seq.item 2 il)
-    )
+  |> Seq.filter triangleEvaluator
 
 (* Answer for Day 3 Part 1 *)
-let day3part1 = 
+let day3part1 =
   possibleTrianglesCount possibleTriangle
   |> Seq.length
 
@@ -71,42 +63,25 @@ In your puzzle input, and instead reading by columns, how many of the listed tri
 (* Function to group values in input file vertically, then evaluate as
   triangles *)
 let possibleVerticalTrianglesCount triangleEvaluator =
-  (* Read in the file and convert all values to ints *)
-  let readInts =
-    File.ReadLines @"3\puzzle_input"
-    |> Seq.map
-      ((fun line -> Regex.Matches (line, @"[0-9]+"))
-      >> Seq.cast
-      >> Seq.map
-        (fun (x : System.Text.RegularExpressions.Match) -> x.Value |> int)
-      )
   (* Turn 3 columns of ints into 1 column *)
-  let colOfInts ints = 
-    ints 
+  let colOfInts ints =
+    ints
     |> Seq.map (Seq.item 0)
     |> Seq.append (ints |> Seq.map (Seq.item 1))
     |> Seq.append (ints |> Seq.map (Seq.item 2))
-  (* Now divide 1 long column of ints into groups of 3 ints *)
-  let horizInts colInts = 
-    colInts
-    |> Seq.windowed 3
-    |> Seq.mapi (fun i elt -> i, elt |> Array.toSeq)
-    |> Seq.filter (fun (i, elt) -> i % 3 = 0)
-    |> Seq.map (fun (i, elt) -> elt)
-  (* Read the data, serialize it, group it again, then filter using 
-    evaluation function *)
-  readInts
-  |> colOfInts
-  |> horizInts
-  |> Seq.filter 
-    (fun il -> 
-      triangleEvaluator 
-        (Seq.item 0 il) 
-        (Seq.item 1 il)
-        (Seq.item 2 il)
+  (* Read the data, serialize it, re-group it, then filter using evaluation function *)
+  File.ReadLines @"./puzzle_input"
+  |> Seq.map
+    ((fun line -> Regex.Matches (line, @"[0-9]+"))
+    >> Seq.cast
+    >> Seq.map
+      (fun (x : System.Text.RegularExpressions.Match) -> x.Value |> int)
     )
+  |> colOfInts
+  |> Seq.chunkBySize 3
+  |> Seq.filter triangleEvaluator
 
 (* Answer for Day 3 Part 2 *)
-let day3part2 = 
+let day3part2 =
   possibleVerticalTrianglesCount possibleTriangle
   |> Seq.length
